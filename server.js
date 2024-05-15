d4184ceb351c19e83a11b9f8526ef0c9067ac1ac
Kreshnik Sadriu <kreshnik@sadriu.ch>
2024-05-16 01:10:37 +0200

    weiteres update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello FS2024 DevOps Course! :-))) Dieser Kurs wurde von Kreshnik Sadriu besuch. mach mal was');
+	res.send('Hello FS2024 DevOps Course! :-))) Nächster Test von kreshnik');
 });
 
 app.listen(PORT, HOST);
